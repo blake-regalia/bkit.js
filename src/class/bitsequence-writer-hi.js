5340173409f73e9e59627207aad8438c0c3d516c
Blake Regalia <blake.regalia@gmail.com>
2019-10-13 11:04:35 -0700

    lo/hi bitsequence writers, selectors and u/int read/writes

---
+++ src/class/bitsequence-writer-hi.js
@@ -0,0 +1,18 @@
+const BitsequenceWriter = require('../class/bitsequence-writer.js');
+
+module.exports = class BitsequenceWriterHi extends BitsequenceWriter {
+	advance(n_bits) {
+		let i_bit = n_bits + this._i_bit;
+		let nb_move = i_bit >> 3;
+		if(nb_move) {
+			if(this._xb_pending) {
+				this._at_contents[this._ib_index] = this._xb_pending;
+				this._xb_pending = 0;
+			}
+			this._ib_index += nb_move;
+		}
+
+		let i_pos = this._i_bit = i_bit % 8;
+		this._xb_pending |= 0x80 >>> i_pos;
+	}
+};
--- src/class/bitsequence-writer.js
+++ src/class/bitsequence-writer.js
@@ -10,51 +10,10 @@ module.exports = class BitsequenceWriter {
 		});
 	}
 
-	advance(n_bits) {
-		let i_bit = n_bits + this._i_bit;
-		let nb_move = i_bit >> 3;
-		if(nb_move) {
-			if(this._xb_pending) {
-				this._at_contents[this._ib_index] = this._xb_pending;
-				this._xb_pending = 0;
-			}
-			this._ib_index += nb_move;
-		}
-
-		let i_pos = this._i_bit = i_bit % 8;
-		this._xb_pending |= 0x80 >>> i_pos;
-	}
-
 	close() {
 		let at_contents = this._at_contents;
 		if(this._ib_index > at_contents.length) throw new Error('bitsequence wrote out of range');
 		at_contents[this._ib_index] = this._xb_pending;
 		return at_contents;
 	}
-
-	// exportRRR(ni_block, ni_super) {
-	// 	ni_block = BigInt(ni_block);
-	// 	ni_super = BigInt(ni_super);
-
-	// 	let ni_sequence = this._n_bits;
-
-	// 	let n_classes = 1 << ni_block;
-	// 	let a_popcounts = [];
-	// 	{
-	// 		for(let i=0; i<=n_classes; i++) {
-	// 			a_popcounts.push(ATU8_EMPTY);
-	// 		}
-	// 	}
-
-	// 	{
-	// 		for(let x_value=0; x_value<n_classes; x_value++) {
-	// 			let c_pop = popcount_uint32(x_value);
-	// 			a_popcounts[c_pop].push();
-	// 		}
-	// 	}
-
-	// 	for(let ii_block=0n; ii_block<ni_sequence; ii_block++) {
-	// 		let x_block = 
-	// 	}
-	// }
 };
